69b1715f1380a373dac9c492d3b69040056be70e
Florian Kulig <florian.kulig@capgemini.com>
2023-12-03 12:11:43 +0100

    close sidebar on bg click

--- client/src/app/layout/layout.component.ts
+++ client/src/app/layout/layout.component.ts
@@ -2,7 +2,6 @@ import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { SidebarComponent } from "./sidebar/sidebar.component";
 import { HeaderComponent } from "./header/header.component";
-import { OutsideClickDirective } from "../directives/outside-click.directive";
 
 @Component({
   selector: "app-layout",
