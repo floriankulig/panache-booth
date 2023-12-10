f528d3196ff1e876e925f031c6316a6d01e6924b
Florian Kulig <florian.kulig@capgemini.com>
2023-12-10 14:14:58 +0100

    make logo separate component

---
+++ client/src/app/components/logo/logo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pb-logo',
+  standalone: true,
+  imports: [],
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.scss'
+})
+export class LogoComponent {
+
+}
--- client/src/app/layout/sidebar/sidebar.component.ts
+++ client/src/app/layout/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { EventEmitter, Component, Output } from "@angular/core";
 import { IconsModule } from "../../icons/icons.module";
+import { LogoComponent } from "../../components/logo/logo.component";
 
 @Component({
   selector: "pb-sidebar",
   standalone: true,
-  imports: [IconsModule],
+  imports: [IconsModule, LogoComponent],
   templateUrl: "./sidebar.component.html",
   styleUrl: "./sidebar.component.scss",
 })
