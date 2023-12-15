1d13d15b941ffe2dd6f1ce78f89aa90ed44516df
Florian Kulig <florian.kulig@capgemini.com>
2023-12-15 12:00:34 +0100

    Update modal and warehouse components

--- client/src/app/pages/warehouse/warehouse.component.ts
+++ client/src/app/pages/warehouse/warehouse.component.ts
@@ -1,11 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 import { IconsModule } from "../../icons/icons.module";
+import { ModalComponent } from "../../components/modal/modal.component";
 
 @Component({
   selector: "pb-warehouse",
   standalone: true,
-  imports: [IconsModule],
+  imports: [IconsModule, ModalComponent],
   templateUrl: "./warehouse.component.html",
   styleUrl: "./warehouse.component.scss",
 })
-export class WarehouseComponent {}
+export class WarehouseComponent {
+  addProductModalOpen = signal(false);
+
+  openAddProductModal() {
+    this.addProductModalOpen.set(true);
+  }
+}
