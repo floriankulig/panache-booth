a81ce8136135e51e50e79b368115b135e5397211
Florian Kulig <florian.kulig@capgemini.com>
2023-12-19 23:17:39 +0100

    implement search

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component, computed } from "@angular/core";
+import { Component, computed, signal } from "@angular/core";
 import { ProductCardComponent } from "../../components/product";
 import { ProductService } from "../../services";
+import { ActivatedRoute } from "@angular/router";
+import { filterByString } from "../../../helpers";
+import { Product } from "../../../models";
 
 @Component({
   selector: "pb-home",
@@ -10,11 +13,44 @@ import { ProductService } from "../../services";
   styleUrl: "./home.component.scss",
 })
 export class HomeComponent {
-  shownProducts = computed(() =>
-    this.productService.products().filter((product) => product.isVisible),
+  searchFilter = signal("");
+
+  shownProducts = computed(
+    () =>
+      filterByString(
+        this.productService
+          .products()
+          .filter((product) => product.isVisible)
+          .map((product) => ({
+            ...product,
+            vendorName: product.vendor.userName,
+            categoryName: product.category.displayValue,
+          })),
+        this.searchFilter(),
+        {
+          include: [
+            "name",
+            "vendorName",
+            "categoryName",
+            "description",
+            "price",
+          ],
+        },
+      ).map((prod) => {
+        const { vendorName, categoryName, ...product } = prod;
+        return product;
+      }) as Product[],
   );
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+  ) {
+    this.route.queryParams.subscribe((params) => {
+      const search = params["q"];
+      this.searchFilter.set(search || "");
+    });
+  }
 
   updateList() {
     this.productService.getProducts();
