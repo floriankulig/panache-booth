6e7fe8749d19c9cae896730278ea0f9497960ddb
lars <60133913+lars-1503@users.noreply.github.com>
2023-12-20 17:26:41 +0100

    Remove unused files

--- server/models/orderProduct.ts
+++
@@ -1,7 +0,0 @@
-import { database } from "./databases";
-import { IOrder } from "./IOrder";
-import { IProduct } from "./IProduct";
-
-export function getOrderProductById(orderId: string, productId: string) {
-  let orderProduct = database.prepare("select ");
-}
--- server/services/orderProduct.ts
+++
@@ -1,5 +0,0 @@
-import { IOrderProduct } from "../models/IOrderProduct";
-
-export function orderProductById(orderId: string, productId: string) {}
-
-export function addOrderProduct(orderProduct: IOrderProduct) {}
