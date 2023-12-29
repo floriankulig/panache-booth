4dee51f82d758b9b914bbf053279f45de4b760a3
lars <60133913+lars-1503@users.noreply.github.com>
2023-12-29 13:26:17 +0100

    Fix for login (no basic auth yet)

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -34,12 +34,14 @@ router.get("/:userId", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    res.status(200).json(await loginUser(req.body));
+    res.status(200).json(loginUser(req.body));
   } catch (error) {
+    console.log(error)
     if (error instanceof UserError) {
       res.status(400).send(error.message);
+    } else {
+      res.status(500).send("Internal server error!");
     }
-    res.status(500).send("Internal server error!");
   }
 });
 
