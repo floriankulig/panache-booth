e087bf11ab1aa8312bdc83aa57a8e7925a772354
Florian Kulig <florian.kulig@capgemini.com>
2024-01-07 16:58:39 +0100

    Add animation to profile menu

--- client/src/app/layout/header/profile-menu/profile-menu.component.ts
+++ client/src/app/layout/header/profile-menu/profile-menu.component.ts
@@ -1,4 +1,11 @@
-import { signal, Component, ElementRef, computed, OnInit } from "@angular/core";
+import {
+  signal,
+  Component,
+  ElementRef,
+  computed,
+  OnInit,
+  HostBinding,
+} from "@angular/core";
 import { IconsModule } from "../../../icons/icons.module";
 import { RouterLink } from "@angular/router";
 import { AuthService } from "../../../services/auth/auth.service";
@@ -6,6 +13,14 @@ import { getDistanceToDate } from "../../../../helpers";
 import { isToday } from "date-fns";
 import { User } from "../../../../models";
 import { CartService, OrderService } from "../../../services";
+import {
+  animate,
+  animateChild,
+  query,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
 
 @Component({
   selector: "pb-profile-menu",
@@ -13,6 +28,43 @@ import { CartService, OrderService } from "../../../services";
   imports: [IconsModule, RouterLink],
   templateUrl: "./profile-menu.component.html",
   styleUrl: "./profile-menu.component.scss",
+  animations: [
+    trigger("menu", [
+      transition("* <=> *", [
+        query("@menuAnimation", animateChild(), {
+          optional: true,
+        }),
+      ]),
+    ]),
+    trigger("menuAnimation", [
+      transition(":enter", [
+        style({
+          opacity: 0,
+          scale: 0,
+          y: -50,
+        }),
+        animate(
+          "300ms cubic-bezier(0.645, 0.045, 0.355, 1)",
+          style({
+            opacity: 1,
+            scale: 1,
+            y: 0,
+          }),
+        ),
+      ]),
+      transition(":leave", [
+        style({ opacity: 1, scale: 1 }),
+        animate(
+          "200ms cubic-bezier(0.645, 0.045, 0.355, 1)",
+          style({
+            opacity: 0,
+            scale: 0.5,
+            y: -50,
+          }),
+        ),
+      ]),
+    ]),
+  ],
 })
 export class ProfileMenuComponent implements OnInit {
   open = signal(false);
@@ -37,6 +89,10 @@ export class ProfileMenuComponent implements OnInit {
     private cartService: CartService,
     private orderService: OrderService,
   ) {}
+
+  @HostBinding("@menu") get animation() {
+    return true;
+  }
 
   ngOnInit() {
     document.addEventListener("click", (event) => {
