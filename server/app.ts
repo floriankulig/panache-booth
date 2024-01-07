ed94091b4e1d2fa896245d2309e83a5c9c117f57
lars <60133913+lars-1503@users.noreply.github.com>
2024-01-07 14:56:37 +0100

    change app env

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,20 @@
-import './util/load-env';
-import express from 'express';
-import path from 'node:path';
-import {apiRouter} from "./api";
+import "./util/load-env";
+import express from "express";
+import path from "node:path";
+import { apiRouter } from "./api";
 
 const app = express();
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
+//app.use(express.static(process.env.FRONTEND_DIST_PATH!));
 app.use(express.static("../client/src"));
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../client/src/index.html"))
+  //res.sendFile(path.join(__dirname, process.env.FRONTEND_DIST_PATH!, "index.html"));
+  res.sendFile(path.join(__dirname, "../client/src/index.html"));
 });
 
 app.listen(process.env.NODE_PORT, () => {
-  console.log(`App listening at http://localhost:${process.env.NODE_PORT}`)
+  console.log(`App listening at http://localhost:${process.env.NODE_PORT}`);
 });
 
